1c3ce661c1fe7bfc3e447f8ba4fee15d4e207f82
Guimachancoses <102677245+Guimachancoses@users.noreply.github.com>
2023-09-18 11:38:10 -0300

    ok

--- BotInsert/BotInsert/bot.py
+++ BotInsert/BotInsert/bot.py
@@ -21,48 +21,50 @@ class Bot(DesktopBot):
             ncard = str(tabela.loc[linha, 'Nº - Cartão'])
             rg = str(tabela.loc[linha, 'RG'])
 
-        if not self.find("novo", matching=0.97, waiting_time=10000):
-            self.not_found("novo")
-        self.click()
+            if not self.find( "novo", matching=0.97, waiting_time=10000):
+                self.not_found("novo")
+            self.click()
 
-        if not self.find("matricula", matching=0.97, waiting_time=10000):
-            self.not_found("matricula")
-        self.click()
-        pg.write(matricula, interval=0.25)
+            if not self.find( "matricula", matching=0.97, waiting_time=10000):
+                self.not_found("matricula")
+            self.click()
+            pg.write(matricula, interval=0.25)
 
-        if not self.find("name_user", matching=0.97, waiting_time=10000):
-            self.not_found("name_user")
-        self.click()
-        self.paste(nome)
+            if not self.find( "name_user", matching=0.97, waiting_time=10000):
+                self.not_found("name_user")
+            self.click()
+            self.paste(nome)
 
-        if not self.find("nc", matching=0.97, waiting_time=10000):
-            self.not_found("nc")
-        self.click()
-        pg.write(ncard, interval=0.25)
+            if not self.find( "nc", matching=0.97, waiting_time=10000):
+                self.not_found("nc")
+            self.click()
+            pg.write(ncard, interval=0.25)
 
-        if not self.find("rg", matching=0.97, waiting_time=10000):
-            self.not_found("rg")
-        self.click()
-        pg.write(rg, interval=0.25)
+            if not self.find( "rg", matching=0.97, waiting_time=10000):
+                self.not_found("rg")
+            self.click()
+            pg.write(rg, interval=0.25)
 
-        if not self.find("jor_click", matching=0.97, waiting_time=10000):
-            self.not_found("jor_click")
-        self.click()
+            if not self.find( "jor_click", matching=0.97, waiting_time=10000):
+                self.not_found("jor_click")
+            self.click()
 
-        if not self.find("jor_default", matching=0.97, waiting_time=10000):
-            self.not_found("jor_default")
-        self.click()
-        if not self.find("confir", matching=0.97, waiting_time=10000):
-            self.not_found("confir")
-        self.click()
+            if not self.find( "jor_default", matching=0.97, waiting_time=10000):
+                self.not_found("jor_default")
+            self.click()
+            if not self.find( "confir", matching=0.97, waiting_time=10000):
+                self.not_found("confir")
+            self.click()
 
-        if not self.find("save", matching=0.97, waiting_time=10000):
-            self.not_found("save")
-        self.click()
+            if not self.find( "save", matching=0.97, waiting_time=10000):
+                self.not_found("save")
+            self.click()
+            sleep(2)
 
-        if not self.find("sair", matching=0.97, waiting_time=10000):
-            self.not_found("sair")
-        self.click()
+            if not self.find( "sair", matching=0.97, waiting_time=10000):
+                self.not_found("sair")
+            self.click()
+            sleep(2)
 
     def not_found(self, label):
         print(f"Element not found: {label}")
